67824ce19da43cd3e27c12a32bb5b3f707c8f8eb
evgenii mikhailishin <djpon94@gmail.com>
2020-02-06 21:12:49 +0300

    Add separate webpack configs, add source maps for dev mode

---
+++ frontend/webpack.common.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.tsx',
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js']
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.join(__dirname, '/dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader'
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+    ],
+};
