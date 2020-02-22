a1a33bf9d0250dc40665f8f146d0748cff804599
evgenii mikhailishin <djpon94@gmail.com>
2020-02-22 12:31:24 +0300

    Make SettingsDrawer width responsive to the screen size

--- frontend/src/components/SettingsDrawer/SettingsDrawer.tsx
+++ frontend/src/components/SettingsDrawer/SettingsDrawer.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { AppState } from '../../Models';
-import { Drawer } from '@material-ui/core';
+import { Drawer, List, ListItem, Slider, makeStyles } from '@material-ui/core';
 import { closeSettingsDrawer } from '../../Actions';
+
+import "./SettingsDrawer.css"
 
 export const SettingsDrawer = () => {
     const isOpened = useSelector((state: AppState) => state.settingsDrawerOpened);
@@ -13,9 +15,37 @@ export const SettingsDrawer = () => {
         [dispatch]
     );
 
+    const useStyles = makeStyles({
+        drawerPaper: {
+            width: '30%',
+            minWidth: '150px'
+        },
+    });
+
+    const classes = useStyles();
+
     return (
-        <Drawer anchor="right" open={isOpened} onClose={closeSettings}>
-            <div>hi there</div>
+        <Drawer
+            anchor="right"
+            open={isOpened}
+            onClose={closeSettings}
+            classes={{
+                paper: classes.drawerPaper,
+            }}
+        >
+            <div className='settings-container'>
+                <List>
+                    <ListItem>
+                        <Slider
+                            value={50}
+                            // onChange={handleSliderChange}
+                            aria-labelledby="input-slider"
+                            max={100}
+                            min={0}
+                        />
+                    </ListItem>
+                </List>
+            </div>
         </Drawer>
     );
 }
